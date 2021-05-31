2314766d12443a7f148ebc00f3d1454f83b4c3c2
EdiCasi <80850475+EdiCasi@users.noreply.github.com>
2021-05-31 21:29:31 +0300

    Lit changes

--- Server/src/main/java/Services/Chat.java
+++ Server/src/main/java/Services/Chat.java
@@ -8,7 +8,7 @@ import proto.ChatOuterClass;
 
 public class Chat extends ChatGrpc.ChatImplBase
 {
-    ChatOuterClass.ChatLog currentMessage;
+    ChatOuterClass.ChatLog currentMessage = ChatOuterClass.ChatLog.newBuilder().build();
 
     @Override
     public void write(ChatOuterClass.ChatLog request, StreamObserver<Empty> responseObserver)
@@ -24,6 +24,8 @@ public class Chat extends ChatGrpc.ChatImplBase
         ChatOuterClass.ChatLog.Builder response = ChatOuterClass.ChatLog.newBuilder().setName(currentMessage.getName()).setMessage(currentMessage.getMessage()).setTime(currentMessage.getTime());
         responseObserver.onNext(response.build());
         responseObserver.onCompleted();
+        System.out.println(response.getMessage());
+
     }
 
     @Override
